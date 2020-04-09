b30874f9a6218afb4686ad17bbb08164175e1c08
Troy Wiegand <troy@troywiegand.com>
2020-04-09 18:30:00 -0400

    Add Tooltips Underneath Keyboard

--- createHTML.js
+++ createHTML.js
@@ -1,8 +1,9 @@
 let ejs = require('ejs')
 let fs = require('fs')
-let rows = [["~","1","2","3","4","5","6","7","8","9","0","-","=","Backspace"],
+let rows = [ ["Escape","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","Home","End","Insert","Delete"],
+            ["`","1","2","3","4","5","6","7","8","9","0","-","=","Backspace"],
             ["Tab","Q","W","E","R","T","Y","U","I","O","P","[","]","\\"],
-            ["Caps","A","S","D","F","G","H","J","K","L",";","'","Enter"],
+            ["Caps","A","S","D","F","G","H","J","K","L",";","'","Return"],
             ["Shift","Z","X","C","V","B","N","M",",",".","/"],
             ["Fn","Ctrl","Super","Alt","Space"]]
 
